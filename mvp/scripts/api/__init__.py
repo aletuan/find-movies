5471ab4f6f02ea2d0cb531b3d7f4f495a44c12c8
Tuan Anh Le <anhlt1983@gmail.com>
2025-03-22 16:24:40 +0700

    feat: remove file

--- mvp/scripts/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# API package for the Movie Search Script 
-from . import tmdb
-from . import omdb
-from . import youtube
-from . import wikipedia 
--- mvp/scripts/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# Utilities package for the Movie Search Script 
-from . import translator
-from . import formatter 
